5b10ded5770f890e34441488b115433cb288df71
Luis Fernando do Rosario Cintra <82674962+fernandocintra2871@users.noreply.github.com>
2024-04-25 16:43:47 -0300

    broker att

--- broker.py
+++ broker.py
@@ -1,52 +1,83 @@
+from flask import Flask, jsonify, request
 import socket
 import threading
+import json
 
-class Broker:
-    def __init__(self):
-        self.messages = []
 
-    def start(self):
-        # Inicia o servidor em uma thread separada
-        server_thread = threading.Thread(target=self.run_server)
-        server_thread.start()
+app = Flask(__name__)
 
-    def run_server(self):
-        # Configura o servidor para ouvir conexões
-        host = 'localhost'
-        port = 12345
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind((host, port))
-        server_socket.listen(5)
+data = []
+devices = [] # hosts conectadas ao broker
+conns = []
+commands = []
 
-        print("Broker aguardando conexões...")
+@app.route('/commands', methods=['POST'])
+def post_command():
+    if request.method == 'POST':
+        new_command = request.json
+        commands.append(new_command)
+        print(commands)
+        return jsonify(commands), 201
 
-        while True:
-            # Aceita conexões dos clientes
-            client_socket, addr = server_socket.accept()
-            print('Conectado por', addr)
-            # Inicia uma thread para lidar com o cliente
-            client_handler = threading.Thread(target=self.handle_client, args=(client_socket,))
-            client_handler.start()
+@app.route('/sensors', methods=['GET'])
+def get_sensors():
+    if request.method == 'GET':
+        return jsonify(data)
 
-    def handle_client(self, client_socket):
-        while True:
-            # Recebe dados do cliente
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            message = data.decode()
-            # Armazena a mensagem recebida
-            self.messages.append(message)
-            print("Mensagem recebida:", message)
-        # Fecha a conexão com o cliente
-        client_socket.close()
+def udp():
+    HOST = '127.0.0.1'
+    PORT = 12344  
 
-    def send_message(self, message):
-        # Envie a mensagem para todos os clientes conectados
-        for client_socket in self.client_sockets:
-            client_socket.send(message.encode())
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((HOST, PORT))
+    print(f"Servidor UDP esperando na porta {PORT}...")
 
-# Exemplo de uso do broker
-if __name__ == "__main__":
-    broker = Broker()
-    broker.start()
+    while True:
+        msg, addr = s.recvfrom(1024)
+        msg = msg.decode()
+        msg = json.loads(msg)
+        if addr not in devices:
+            devices.append(addr)
+            data.append(msg)
+            print(f"Nova conexão feita  com {addr}\nDados recebidos: {msg}")
+        else:
+            index = devices.index(addr)
+            data[index] = msg
+            print(f"{msg} recebidos de {addr}")
+
+def tcp_listener(s):
+    while True:
+        s.listen()
+        print("Aguardando conexão...")
+        conn, addr = s.accept()
+        msg = conn.recv(1024)
+        if msg.decode() == 'init':
+            conns.append(conn)
+            print(f"Conexão TCP {conn} feita com {addr}")
+
+def tcp_send(s):
+    while True:
+        if len(commands) > 0:
+            conns[commands[0]['sensor']].send(commands[0]['command'].encode())
+            print(f"Comando {commands[0]['command']} enviado para o Sensor {commands[0]['sensor']}")
+            if commands[0]['command'] == 'desligar':
+                data[commands[0]['sensor']]['state'] = False
+            commands.pop(0)
+
+
+thread_udp = threading.Thread(target=udp)
+thread_udp.start()
+
+HOST = '127.0.0.1'
+PORT = 12343
+
+tcp_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_s.bind((HOST, PORT))
+
+thread_tcp_listener = threading.Thread(target=tcp_listener, args=(tcp_s,))
+thread_tcp_listener.start()
+
+thread_tcp_send = threading.Thread(target=tcp_send, args=(tcp_s,))
+thread_tcp_send.start()
+
+app.run(host='127.0.0.1', port=12345)
